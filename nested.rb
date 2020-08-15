de84ca5c28c8a0f406995f7ca15e7f48fafb516e
McKayla Lankau <lankaukk@yahoo.com>
2020-08-15 20:04:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,7 +100,10 @@ def adding_matz
   # languages = yukihiro_matsumoto[:languages]
   # languages << "LISP" "C"
 
-  programmer_hash = {:yukihiro_matsumoto
+  programmer_hash = {:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}}
 
   programmer_hash
     
